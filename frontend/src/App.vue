66400e8049e4d2bc5399ae9057177f9cbb53d985
dhyeyd1 <137206127+dhyeyd1@users.noreply.github.com>
2023-06-20 17:45:43 +0000

    removed hello world component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,10 +8,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/qrcode.png" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
