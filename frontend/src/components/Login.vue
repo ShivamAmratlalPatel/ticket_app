10d58f54b90f1a74cd84d4c46cde5031fd129ba4
dhyeyd1 <137206127+dhyeyd1@users.noreply.github.com>
2023-06-20 17:37:33 +0000

    added login file

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<style></style>
+
+<script>
+export default {
+    data() {
+        return {
+            isLoggedIn: false
+        }
+    },
+    methods: {
+        changeLogin() {
+            return false
+        }
+    }
+}
+</script>
